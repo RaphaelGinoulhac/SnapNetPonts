c8acb12a76ed08e6b2962c7030ede484e5fbf3eb
Raphael <raphael.ginoulhac@gmail.com>
2018-05-09 11:48:32 +0200

    Commented test files

--- sem3d_gen_images.py
+++ sem3d_gen_images.py
@@ -66,20 +66,20 @@ if(config["training"]):
 else: # testing filename
     filenames = [
             "birdfountain_station1_xyz_intensity_rgb",
-             "castleblatten_station1_intensity_rgb",
-             "castleblatten_station5_xyz_intensity_rgb",
-             "marketplacefeldkirch_station1_intensity_rgb",
-             "marketplacefeldkirch_station4_intensity_rgb",
-             "marketplacefeldkirch_station7_intensity_rgb",
-             "sg27_station10_intensity_rgb",
-             "sg27_station3_intensity_rgb",
-             "sg27_station6_intensity_rgb",
-             "sg27_station8_intensity_rgb",
-             "sg28_station2_intensity_rgb",
-             "sg28_station5_xyz_intensity_rgb",
-             "stgallencathedral_station1_intensity_rgb",
-             "stgallencathedral_station3_intensity_rgb",
-             "stgallencathedral_station6_intensity_rgb"
+            # "castleblatten_station1_intensity_rgb",
+            # "castleblatten_station5_xyz_intensity_rgb",
+            # "marketplacefeldkirch_station1_intensity_rgb",
+            # "marketplacefeldkirch_station4_intensity_rgb",
+            # "marketplacefeldkirch_station7_intensity_rgb",
+            # "sg27_station10_intensity_rgb",
+            # "sg27_station3_intensity_rgb",
+            # "sg27_station6_intensity_rgb",
+            # "sg27_station8_intensity_rgb",
+            # "sg28_station2_intensity_rgb",
+            # "sg28_station5_xyz_intensity_rgb",
+            # "stgallencathedral_station1_intensity_rgb",
+            # "stgallencathedral_station3_intensity_rgb",
+            # "stgallencathedral_station6_intensity_rgb"
         ]
 
 
