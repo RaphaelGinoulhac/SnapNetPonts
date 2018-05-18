fbde88ab9d1dc5e7e246136cb0edd18e6509b00f
Raphaelhack <raphael.ginoulhac@gmail.com>
2018-05-18 16:35:13 +0200

    Ajustement des path de setup.py pour Rio.

--- pointcloud_tools/setup.py
+++ pointcloud_tools/setup.py
@@ -8,6 +8,7 @@ pcl_lib_dir = "/usr/local/lib"
 vtk_include_dir = "/usr/include/vtk-6.2"
 eigen_include_dir = "/usr/include/eigen3"
 flann_include_dir = "/home/eleves/Documents/nanoflann"
+numpy_include_dir = "/home/eleves/anaconda3/envs/tf/lib/python2.7/site-packages/numpy/core/include"
 
 # depending on the distribution, change the directory according to the installed version
 # example for stock ubuntu 16.04:
@@ -16,7 +17,7 @@ flann_include_dir = "/home/eleves/Documents/nanoflann"
 ext_modules = [Extension(
        "PcTools",
        sources=["Semantic3D.pyx", "Sem3D.cxx","pointCloud.cxx", "pointCloudLabels.cxx"],  # source file(s)
-       include_dirs=["third_party_includes/",pcl_include_dir, vtk_include_dir, eigen_include_dir, flann_include_dir],
+       include_dirs=["third_party_includes/",pcl_include_dir, vtk_include_dir, eigen_include_dir, flann_include_dir, numpy_include_dir],
        language="c++",             # generate C++ code
        library_dirs=[pcl_lib_dir],
        libraries=["pcl_common","pcl_kdtree","pcl_features","pcl_surface","pcl_io"],
